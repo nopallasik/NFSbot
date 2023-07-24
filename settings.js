83e2d37a06e3a05bd4410a10684ffffcca3ab8fb
Blawuken <jarbull87@gmail.com>
2023-07-24 18:41:14 +0700

    translate global mess

--- settings.js
+++ settings.js
@@ -61,7 +61,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 
 //messages
 global.mess = {
-    success: 'Here you go!', 
+    success: 'Ini dia!', 
 }
 
 let file = require.resolve(__filename)
